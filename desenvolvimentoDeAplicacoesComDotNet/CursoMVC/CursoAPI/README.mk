f05e66be07aafff618ce41d104b75b9113623d19
lucasAlex-sys <lucas.alex@hotmail.com.br>
2021-06-20 21:50:13 -0300

    Desenvolvimento de aplicações com .net concluida

---
+++ desenvolvimentoDeAplicacoesComDotNet/CursoMVC/CursoAPI/README.mk
@@ -0,0 +1,8 @@
+# Desenvolvimento de aplicações com .Net
+
+### Neste repositorio está contido aplicações ASP .NET API e ASP .NET MVC, onde foi utilizado:
+
+### Entity frameowork: É um mapeador relacional de objeto que permmite que os desenvolvedores de .NET
+### trabalhem com um banco de dados usando objetos .NET
+### Swagger: É um projeto composto por algumas ferramentas para auxiliar o desenvolvedor de API em algumas
+### tarefas, como por exemplo, a documentação da API.
